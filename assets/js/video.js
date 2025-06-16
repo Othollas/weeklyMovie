0c3727b449043797ddbe6aac52b294138c886b31
o_thollas <o_thollas@hotmail.com>
2025-06-16 15:30:20 +0200

    Creation fichier video.js afin de stocké temporairement les infos des videos

---
+++ assets/js/video.js
@@ -0,0 +1,11 @@
+let movies = [
+  { "nom": "Film1", "videoName": "aAyQX8Z_460svav1", "tracks": [] },
+  { "nom": "Film2", "videoName": "adB2EZZ_460svav1", "tracks": [] },
+  { "nom": "Film3", "videoName": "agmONYx_460svav1", "tracks": [] },
+  { "nom": "Film4", "videoName": "apR93On_460svav1", "tracks": [] },
+  { "nom": "Film5", "videoName": "aqyjv5Q_460svav1", "tracks": [] },
+  { "nom": "Film6", "videoName": "aRBLExA_460svav1", "tracks": [] },
+  { "nom": "Film7", "videoName": "aVvEXOd_460svav1", "tracks": [] }
+];
+
+
